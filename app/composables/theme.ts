50c91c81b43bc191fd84b5e37089c7a78ef447c3
ybc <10533897+ybcybc@user.noreply.gitee.com>
2025-09-09 18:41:17 +0800

    111

--- app/composables/theme.ts
+++ app/composables/theme.ts
@@ -1,3 +1,8 @@
-import {ref} from 'vue'
+import { ref } from "vue";
 
-export const theme = ref<'light' | 'dark'>('light')
+export const theme = ref<any>({ label: "light", color: "#f6f1ed" });
+export const changeTheme = () => {
+  theme.value.label == "light"
+    ? (theme.value = { label: "dark", color: "#1e1e1e" })
+    : (theme.value = { label: "light", color: "#f6f1ed" });
+};
--- app/pages/home.vue
+++ app/pages/home.vue
@@ -1,18 +1,56 @@
 <template>
-    <main class="layout">
-        <aside class="sidebar">
-            
-        </aside>
-        <section class="content">
-            <NuxtPage/>
-        </section>
-    </main>
+  <main class="layout" :style="{ backgroundColor: theme.color }">
+    <aside class="sidebar">
+      <nav class="nav">
+        <NuxtLink to="/">
+          <img class="icon" :src="homeIcon" alt="home" />
+        </NuxtLink>
+        <NuxtLink to="/about">
+          <img class="icon" :src="aboutIcon" alt="" />
+        </NuxtLink>
+        <NuxtLink to="/posts">Posts</NuxtLink>
+        <NuxtLink to="/projects">Projects</NuxtLink>
+      </nav>
+    </aside>
+    <section class="content">
+      <NuxtPage />
+    </section>
+  </main>
 </template>
 <script setup>
+import { theme } from "~/composables/theme";
+import { homeIcon, aboutIcon } from "~/assets/icon/svg";
 </script>
 <style lang="css" scoped>
-.content{
+.layout {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  .sidebar {
+    width: 20vw;
+    height: 100vh;
+    background-color: #f6f1ed;
+    position: fixed;
+  }
+  .content {
     width: 80vw;
     height: 100vh;
+    background-color: #f6f1ed;
+  }
+}
+.nav {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.icon {
+  width: 20px;
+  height: 20px;
+  vertical-align: middle;
 }
 </style>
